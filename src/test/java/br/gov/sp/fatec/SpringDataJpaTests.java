a2e7466854992482ae4ca433d71fb2f981c9ed23
daniellygj <daniellygj@gmail.com>
2020-02-21 21:21:04 -0300

    configuração inicial

---
+++ src/test/java/br/gov/sp/fatec/SpringDataJpaTests.java
@@ -0,0 +1,18 @@
+package br.gov.sp.fatec;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@Rollback
+public class SpringDataJpaTests {
+
+
+
+}
